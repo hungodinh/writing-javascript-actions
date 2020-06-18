1949eb146b5d4cf9cfeb0e1286bda4030c522afb
Hung Dinh <hungo.dinh@gmail.com>
2020-06-18 18:15:44 +1000

    update fix

--- .github/actions/hello-world/main.js
+++ .github/actions/hello-world/main.js
@@ -1,8 +1,6 @@
-console.log("hello world");
-
 const core = require("@actions/core");
 
-const firstGreeting = core.getInput("first-greeting"):
+const firstGreeting = core.getInput("first-greeting");
 const secondGreeting = core.getInput("second-greeting");
 const thirdGreeting = core.getInput("third-greeting");
 
